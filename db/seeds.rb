d0d5a8608dd7ab0971d9cc78443516839e379a14
soufianeou <soufiane.oucherrou@gmail.com>
2019-10-22 13:32:36 -0400

    some changes

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+DogOwner.create(username: "Test1", password: "123456", name: "Soufi", phone_number: "6462312553", adress: "brooklyn")
+DogOwner.create(username: "Test2", password: "215365", name: "adam", phone_number: "6462312553", adress: "bayrudge")
+DogOwner.create(username: "Test3", password: "864215", name: "Jeff", phone_number: "6462312553", adress: "no adress")
+DogOwner.create(username: "Test4", password: "321456", name: "lili", phone_number: "6462312553", adress: "empthy")
+DogOwner.create(username: "Test5", password: "102350", name: "unknow", phone_number: "6462312553", adress: "jklj")
+DogOwner.create(username: "Test6", password: "132365", name: "someone", phone_number: "6462312553", adress: "brooklyn")
