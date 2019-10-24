f0276ea6a48c776201d3082007dcb8a2af027fdc
Jeffrey Paulino <jp7kode@gmail.com>
2019-10-23 21:57:31 -0400

    Currently working on creating appointments

--- lib/models/dog_owner.rb
+++ lib/models/dog_owner.rb
@@ -1,5 +1,4 @@
 
-
 class DogOwner < ActiveRecord::Base
   has_many :appointments
   has_many :dog_walkers, through: :appointments
@@ -12,9 +11,11 @@ class DogOwner < ActiveRecord::Base
 
   has_one :account
 
-
   def self.username_password_auth(username, password)
     find_by(username: username, password: password)
   end
 
+  # def self.availability_list
+  #
+  # end
 end
